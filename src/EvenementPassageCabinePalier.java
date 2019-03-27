803c3b1fe69a0f564ae653b2b440839276941cf8
victor <victor.moreliere8@etu.univ-lorraine.fr>
2019-03-27 13:24:07 +0100

    test

--- src/EvenementPassageCabinePalier.java
+++ src/EvenementPassageCabinePalier.java
@@ -26,10 +26,6 @@ public class EvenementPassageCabinePalier extends Evenement {
 		
 		if((cabine.passagersVeulentDescendre()) || (étage.aDesPassagers())) {
 			tps = date + Global.tempsPourBougerLaCabineDUnEtage + Global.tempsPourEntrerOuSortirDeLaCabine * (cabine.nbPassagersVeulentDescendre(cabine.étage.numéro()) + étage.nbPassagersEtage(étage.numéro()) + tempsPourOuvrirOuFermerLesPortes); // +1 ?
-			// étage.arrivéeSuivante() ?
-			//géré en dessous
-			//echeancier.ajouter(new EvenementOuverturePorteCabine(date+1));
-			//echeancier.ajouter(new EvenementFermeturePorteCabine(date+1+(Global.tempsPourEntrerOuSortirDeLaCabine * cabine.nbPassagersVeulentDescendre(cabine.étage.numéro())) ));
 		} else {
 			tps = date + Global.tempsPourBougerLaCabineDUnEtage;
 		}
@@ -40,30 +36,48 @@ public class EvenementPassageCabinePalier extends Evenement {
 		else if((cabine.intention() == '^') && (cabine.étage.numéro() != immeuble.étageLePlusHaut().numéro()))
 			cabine.étage = immeuble.étage(cabine.étage.numéro()+1);
 		
-		if(!étage.aDesPassagers() && !cabine.passagersVeulentDescendre()) {
-			if((cabine.intention() == 'v') && (cabine.étage.numéro() != immeuble.étageLePlusBas().numéro()))
-				echeancier.ajouter(new EvenementPassageCabinePalier(tps, immeuble.étage(cabine.étage.numéro()-1))); // étage.arrivéeSuivante() ?
-			else if((cabine.intention() == '^') && (cabine.étage.numéro() != immeuble.étageLePlusHaut().numéro()))
-				echeancier.ajouter(new EvenementPassageCabinePalier(tps, immeuble.étage(cabine.étage.numéro()+1))); // étage.arrivéeSuivante() ?
-		}
-		//Gestion de l'ouverture des portes + intention
-		
+		boolean monter=false;
 		if(Global.modeParfait && étage.aDesPassagers() && (cabine.intention() == étage.getPremierPassager().sens()) && !cabine.cabinePleine()){
 			echeancier.ajouter(new EvenementOuverturePorteCabine(date + Global.tempsPourOuvrirOuFermerLesPortes));
+			monter=true;
 		}
 		else if(cabine.passagersVeulentDescendre()) {
 			echeancier.ajouter(new EvenementOuverturePorteCabine(date + Global.tempsPourOuvrirOuFermerLesPortes));
+			monter=true;
 		}
 		else if((étage.aDesPassagers()) && (! cabine.cabinePleine())&& ! Global.isModeParfait()){
 			echeancier.ajouter(new EvenementOuverturePorteCabine(date + Global.tempsPourOuvrirOuFermerLesPortes));
+			monter=true;
 		}
 		else {
 			int nb =-1;
 			if(cabine.intention() == '^') nb=immeuble.passagerAuDessus(cabine.étage);
 			else nb = immeuble.passagerEnDessous(cabine.étage);
-			if(étage.aDesPassagers()&&!cabine.cabinePleine()&&nb==-1)echeancier.ajouter(new EvenementOuverturePorteCabine(date + Global.tempsPourOuvrirOuFermerLesPortes));
+			if(étage.aDesPassagers()&&!cabine.cabinePleine()&&nb==-1) {
+				echeancier.ajouter(new EvenementOuverturePorteCabine(date + Global.tempsPourOuvrirOuFermerLesPortes));
+				monter=true;
+			}
 		}
+		//System.out.println(monter);
+		if((!étage.aDesPassagers() || (Global.isModeParfait() && !monter))&& !cabine.passagersVeulentDescendre()) {
+			//System.out.println("ici");
+			if((cabine.intention() == 'v') && (cabine.étage.numéro() != immeuble.étageLePlusBas().numéro())) {
+				//System.out.println("EVENEMNT");
+				//echeancier.affiche(new StringBuilder(),immeuble);
+				echeancier.ajouter(new EvenementPassageCabinePalier(tps, immeuble.étage(cabine.étage.numéro()-1))); // étage.arrivéeSuivante() ?
+				//echeancier.affiche(new StringBuilder(),immeuble);
+			}
+			else if((cabine.intention() == '^') && (cabine.étage.numéro() != immeuble.étageLePlusHaut().numéro())){
+				//System.out.println("EVENEMNT");
+				//echeancier.affiche(new StringBuilder(),immeuble);
+				echeancier.ajouter(new EvenementPassageCabinePalier(tps, immeuble.étage(cabine.étage.numéro()+1))); // étage.arrivéeSuivante() ?
+				//echeancier.affiche(new StringBuilder(),immeuble);
+			}
+		}
+		//Gestion de l'ouverture des portes + intention
+		
+		
     }
-   
-
+ 
+    
 }
